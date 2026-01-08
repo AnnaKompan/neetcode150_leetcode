602bc8a174b7973a93172761467a9b8809207681
Anna Kompan <annakompanch@gmail.com>
2026-01-08 12:53:14 +0200

    Solve: Binary Search -> O(log n)

--- binary_search.py
+++ binary_search.py
@@ -3,6 +3,7 @@ Docstring for binary_search
 Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 You must write an algorithm with O(log n) runtime complexity.
 Time: O(n)
+Space: O(1)
 """
 from typing import List
 class Solution:
@@ -10,4 +11,22 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == target:
                 return i
+        return -1
+    
+"""
+Time: O(log n)
+Space: O(1)
+"""
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums)-1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            elif nums[mid] < target:
+                low = mid + 1
         return -1
