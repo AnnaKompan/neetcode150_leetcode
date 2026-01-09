c52b4889784117cf1a38c8efa232f853a2de4dc4
AnnaKompan <annakompanch@gmail.com>
2026-01-09 23:54:32 +0200

    Solve: Contains Duplicate brute force

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -13,3 +13,16 @@ class Solution:
                 return True
             hashset.add(num)
         return False
+
+"""
+Solution 2 (Brute Force)
+Time: O(n^2)
+Space: O(1)
+"""
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
