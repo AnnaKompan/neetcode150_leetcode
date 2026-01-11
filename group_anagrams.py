19343798ccfb495819c3c03ce7e013ce32ad1b41
AnnaKompan <annakompanch@gmail.com>
2026-01-11 23:59:22 +0200

    Solve: Group Anagrams

---
+++ group_anagrams.py
@@ -0,0 +1,26 @@
+"""
+Docstring for group_anagrams
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+Time: O(n k) where n is number of strings and k is max length of a string
+Space: O(n k)
+Link: https://neetcode.io/problems/group-anagrams/question?list=neetcode150
+"""
+
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 0:
+            return strs
+        anagrams = {}
+        for w in strs:
+            count = [0] * 26
+            for char in w:
+                count[ord(char) - ord('a')] += 1
+            key = tuple(count)
+            if key not in anagrams:
+                anagrams[key] = []
+            anagrams[key].append(w)
+
+        return list(anagrams.values())
