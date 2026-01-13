dabbbbc134ca91eeed3723a3f4aa9444ae9716dd
Anna Kompan <annakompanch@gmail.com>
2026-01-13 21:17:13 +0200

    Solve: Top-k frequent element (Time: n log n)

--- group_anagrams.py
+++ group_anagrams.py
@@ -13,15 +13,13 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) == 0:
             return strs
-        anagrams = {}
+        anagrams = defaultdict(list)
         for w in strs:
             count = [0] * 26
             for char in w:
                 count[ord(char) - ord('a')] += 1
             key = tuple(count)
-            if key not in anagrams:
-                anagrams[key] = []
-            anagrams[key].append(w)
+            anagrams[tuple(count)].append(w)
 
         return list(anagrams.values())
 
