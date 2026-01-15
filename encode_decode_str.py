fd400168d5052e4fffb0c3f4930024406d309171
AnnaKompan <annakompanch@gmail.com>
2026-01-16 00:01:36 +0200

    Solve: encode_decode (O(n)-time)

--- encode_decode_str.py
+++ encode_decode_str.py
@@ -1,5 +1,13 @@
+"""
+Docstring for encode_decode_str
+Design an algorithm to encode a list of strings to a single string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+Note: The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
+Time: O(n) where n is total number of characters in all strings
+Space: O(n)
+Link: https://neetcode.io/problems/encode-and-decode-strings/question?list=neetcode150
+"""
+from ast import List
 class Solution:
-
     def encode(self, strs: List[str]) -> str:
         if not strs:
             return ""
