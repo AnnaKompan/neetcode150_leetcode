deff8a165e19863f844aa1e1ce98287157148787
AnnaKompan <annakompanch@gmail.com>
2026-01-16 01:13:57 +0200

    Solve: encode_decode (refactor)

--- encode_decode_str.py
+++ encode_decode_str.py
@@ -2,40 +2,32 @@
 Docstring for encode_decode_str
 Design an algorithm to encode a list of strings to a single string. The encoded string is then sent over the network and is decoded back to the original list of strings.
 Note: The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
-Time: O(n) where n is total number of characters in all strings
+Time: O(n) where n is the total number of characters in all strings
 Space: O(n)
 Link: https://neetcode.io/problems/encode-and-decode-strings/question?list=neetcode150
 """
 from ast import List
 class Solution:
+
     def encode(self, strs: List[str]) -> str:
         if not strs:
             return ""
-        sizes, res = [], ""
+        result = ""
         for s in strs:
-            sizes.append(len(s))
+            result += str(len(s)) + "#" + s
+        return result
 
-        for sz in sizes:
-            res += str(sz)
-            res += ','
-        res += '#'
-        for s in strs:
-            res += s
-        return res
+        
 
     def decode(self, s: str) -> List[str]:
-        if not s:
-            return []
-        sizes, res, i = [], [], 0
-        while s[i] != '#':
-            cur = ""
-            while s[i] != ',':
-                cur += s[i]
-                i += 1
-            sizes.append(int(cur))
-            i += 1
-        i += 1
-        for sz in sizes:
-            res.append(s[i:i + sz])
-            i += sz
-        return res
+        result, i = [], 0
+        while i < len(str):
+            j = i
+            while str[j] != "#":
+                j += 1
+            length = int(str[i:j])
+            result.append(str[j+1 : j+1+length])
+            i = j+1+length
+        return result
+
+
