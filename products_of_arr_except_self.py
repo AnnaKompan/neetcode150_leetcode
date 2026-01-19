a3a503f4ba64a9def97876191f448491a661f57c
Anna Kompan <annakompanch@gmail.com>
2026-01-19 13:53:31 +0200

    Solve: Product of arr except self Time (O(n))

--- products_of_arr_except_self.py
+++ products_of_arr_except_self.py
@@ -18,3 +18,23 @@ class Solution:
                     dot_prod*=nums[j]
             res.append(dot_prod)
         return res
+"""
+Time: O(n)
+Space: O(1)
+"""
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return 0
+        res = [1] * (len(nums))
+        # [1,1,1,1]
+        pref = 1
+        for i in range(len(nums)):
+            res[i] = pref # res = [1, 1, 2, 8]
+            pref *= nums[i] # pref = 1 / 2 / 8 / 48
+        post = 1
+        for i in range(len(nums)-1,-1, -1):
+            res[i] *= post # res = [48,24,12,8]
+            post *= nums[i] # post = 6 / 24 / 48 / 48
+        return res
+
